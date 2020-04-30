84ad708da86eded0340bf5e3b2872d0f18335063
kimhjona <mac@J.local>
2020-04-29 20:41:31 -0400

    seeing if my username is connected

--- exercises/exercise-04/index.ts
+++ exercises/exercise-04/index.ts
@@ -105,9 +105,9 @@ function logPerson(person: Person) {
 function filterUsers(persons: Person[], criteria: Partial<User>): User[] {
   return persons.filter(isUser).filter((user) => {
     let criteriaKeys = Object.keys(criteria) as (keyof User)[];
-    return criteriaKeys.every((fieldName) => {
-      return user[fieldName] === criteria[fieldName];
-    });
+    return criteriaKeys.every(
+      (fieldName) => user[fieldName] === criteria[fieldName]
+    );
   });
 }
 
